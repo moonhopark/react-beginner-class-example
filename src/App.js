0a03292f7b738669e7af5cc9d83d07c424cabfcb
moonhopark <moonho4199@naver.com>
2023-01-12 10:27:50 +0900

    feat: props, state 예시 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
 import FunctionComponent from './components/FunctionComponent';
 import JSX from './components/JSX';
+import Props from './components/Props';
 import PureJS from './components/PureJS';
+import State from './components/State';
 
 const App = () => {
   return (
     <div className="App">
-      <FunctionComponent />
-      <JSX />
-      <PureJS />
+      {/* <FunctionComponent /> */}
+      {/* <JSX /> */}
+      {/* <PureJS /> */}
+      <State />
+      <Props name="Teo" age="25">
+        <p>This is children props</p>
+      </Props>
+      <Props name="Jason" age="26">
+        <button>Click!</button>
+      </Props>
+      <Props name="Lee" age="27" />
     </div>
   );
 };
