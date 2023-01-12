a1b6640822f83b17333597a45f6d66b6638afd6f
moonhopark <moonho4199@naver.com>
2023-01-12 11:33:48 +0900

    feat: useState 예시 코드 추가

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import JSX from './components/JSX';
 import Props from './components/Props';
 import PureJS from './components/PureJS';
 import State from './components/State';
+import UseState from './components/UseState';
 
 const App = () => {
   return (
     <div className="App">
+      <UseState />
       {/* <FunctionComponent /> */}
       {/* <JSX /> */}
       {/* <PureJS /> */}
-      <State />
+      {/* <State />
       <Props name="Teo" age="25">
         <p>This is children props</p>
       </Props>
       <Props name="Jason" age="26">
         <button>Click!</button>
       </Props>
-      <Props name="Lee" age="27" />
+      <Props name="Lee" age="27" /> */}
     </div>
   );
 };
