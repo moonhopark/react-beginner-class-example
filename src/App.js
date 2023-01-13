822cad6f96e2333662e4ecf5e512da4784d843dc
moonhopark <moonho4199@naver.com>
2023-01-13 14:42:40 +0900

    feat: useReducer 예시 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import './App.css';
-import ConditionalRender from './components/ConditionalRender';
-import FunctionComponent from './components/FunctionComponent';
-import JSX from './components/JSX';
-import Props from './components/Props';
-import PureJS from './components/PureJS';
-import State from './components/State';
-import UseState from './components/UseState';
+
+import UseReducerOne from './components/UseReducerOne';
+import UseReducerTwo from './components/UseReducerTwo';
 
 const App = () => {
   return (
     <div className="App">
-      {/* <UseState /> */}
-      <ConditionalRender />
-      {/* <FunctionComponent /> */}
-      {/* <JSX /> */}
-      {/* <PureJS /> */}
-      {/* <State />
-      <Props name="Teo" age="25">
-        <p>This is children props</p>
-      </Props>
-      <Props name="Jason" age="26">
-        <button>Click!</button>
-      </Props>
-      <Props name="Lee" age="27" /> */}
+      {/* <UseReducerOne /> */}
+      <UseReducerTwo />
     </div>
   );
 };
